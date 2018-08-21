7af76829f556d65b45828f94f343935716cc366d
Johnathanrs <JohnathanS23@yahoo.com>
2018-08-20 17:39:07 -0700

    small change

--- app.py
+++ app.py
@@ -281,7 +281,7 @@ def api_echo():
 #Calls WOYC API based on paramaters.
 @app.route('/WOYC/send_order', methods=['POST'])
 def send_order(order, shipping, products):
-    sys.stdout.write(str(dir(products)))
+    sys.stdout.write(products)
     """
     try:
         total = len(products) - 1
