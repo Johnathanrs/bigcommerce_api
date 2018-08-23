25ad1f0aa8c4760462cf2390d6a2453eb208093a
Johnathanrs <JohnathanS23@yahoo.com>
2018-08-22 19:17:57 -0700

    small changes

--- app.py
+++ app.py
@@ -334,7 +334,8 @@ def send_order(order, shipping, products):
         send_package = requests.post(url, data=send_request, headers=settings)
         sys.stdout.write("****************** send_package() *****************" + "\n")
         sys.stdout.write(str(send_package) + "\n")
-        sys.stdout.write(str(dir(send_package)) + "\n")
+        sys.stdout.write(str(send_package['content']) + "\n")
+        sys.stdout.write(str(send_package['reason']) + "\n")
         '''
         while (send_package.status_code != 200 or send_package.status != 201) and attempts > 0:
             send_package = requests.post(package, json=package, headers=headers)
